bbb4aa5bcbc5baedcb2d657b209d0c1ce7ead496
Augustin Predhomme <raygis@tutanota.com>
2024-11-06 20:21:58 +0100

    Création Home Page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="logo" alt="temtem-lite-logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Home Page
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="tab-link" href="/login">
+          Register
+        </a>
+        <a className="tab-link" href="/register">
+          Login
         </a>
       </header>
     </div>
