bf8155231f7ed73fc65a1230d2cf909aacda934a
Augustin Predhomme <raygis@tutanota.com>
2024-11-14 00:14:09 +0100

    clear all data fix

--- packages/server/src/controllers/debugController.ts
+++ packages/server/src/controllers/debugController.ts
@@ -5,6 +5,9 @@ import { Skill } from "../models/Skill";
 import { Temtem } from "../models/Temtem";
 import { Inventory } from "../models/Inventory";
 import { Trade } from "../models/Trade";
+import { InventoryTemtem } from "../models/InventoryTemtem";
+import { TemtemSkill } from "../models/TemtemSkill";
+import { Fight } from "../models/Fight";
 
 
 export const clearAllData = async (request: Request, response: Response) => {
@@ -13,8 +16,11 @@ export const clearAllData = async (request: Request, response: Response) => {
             User.destroy({truncate: true}),
             Skill.destroy({ truncate: true }),
             Temtem.destroy({ truncate: true }),
+            TemtemSkill.destroy({ truncate: true }),
             Inventory.destroy({ truncate: true }),
-            Trade.destroy({ truncate: true })
+            InventoryTemtem.destroy({ truncate: true }),
+            Trade.destroy({ truncate: true }),
+            Fight.destroy({truncate: true})
         ]);
         return APIResponse(response, [], 'All data has been deleted');
     } catch (error) {
